cf2976e58bd40e8431fd3ed82cbc172ad7b7702a
ISAIAH KOYANI <134575499+isaiahk-ai@users.noreply.github.com>
2026-03-12 00:39:32 +0300

    Add variables, list, and dictionary examples
    Added variables for name, age, and city. Included a list of programming languages and a dictionary for a student.

--- assignment.py
+++ assignment.py
@@ -8,9 +8,9 @@ print("Hello AI Students")
 # Question 5
 # Create variables: name, age, city
 # Print in the format: Name: John   Age: 20   City: Nairobi
-  Name = "John"
-  Age = 20
-  City = "Nairobi"
+Name = "John"
+Age = 20
+City = "Nairobi"
 print(f"Name: {Name}")
 print(f"Age: {Age}")
 print(f"City: {City}")
@@ -18,10 +18,18 @@ print(f"City: {City}")
 # Question 6
 # Create a list of five programming languages
 # Print the first language
+languages=["Python", "C++", "Java", "JavaScript", "Go"]
+print(languages[0])
 
 # Question 7
 # Create a dictionary for a student (Name, Age, Course)
 # Print the student's name
+student = {
+"name": "Isaiah",
+"age": 21,
+"course": "AI"
+}
+print(student["name"])
 
 # Bonus (optional)
 # Explain list vs dictionary difference (you can write this as a comment)

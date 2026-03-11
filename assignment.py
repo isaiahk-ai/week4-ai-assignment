bc8f10f5b51b6a01fa1aeb70beb2dca42ac9329f
ISAIAH KOYANI <134575499+isaiahk-ai@users.noreply.github.com>
2026-03-11 23:52:22 +0300

    Add Python assignment for Week 4

---
+++ assignment.py
@@ -0,0 +1,27 @@
+# Week 4 AI Assignment - Python file
+# Isaiah
+
+# Question 4
+# Write a Python program that prints: Hello AI Students
+print("Hello AI Students")
+
+# Question 5
+# Create variables: name, age, city
+# Print in the format: Name: John   Age: 20   City: Nairobi
+  Name = "John"
+  Age = 20
+  City = "Nairobi"
+print(f"Name: {Name}")
+print(f"Age: {Age}")
+print(f"City: {City}")
+
+# Question 6
+# Create a list of five programming languages
+# Print the first language
+
+# Question 7
+# Create a dictionary for a student (Name, Age, Course)
+# Print the student's name
+
+# Bonus (optional)
+# Explain list vs dictionary difference (you can write this as a comment)
